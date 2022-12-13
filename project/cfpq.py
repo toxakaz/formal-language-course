15b51495fbf1d49bffcd1279d3afc4f7ed905ae2
ToxaKaz <anton.a.kazancev@gmail.com>
2022-12-13 20:02:56 +0300

    task_9 (hellings) done

--- project/cfpq.py
+++ project/cfpq.py
@@ -1,10 +1,76 @@
 from networkx import MultiDiGraph
-from pyformlang.cfg import CFG
+from pyformlang.cfg import CFG, Terminal
 from scipy.sparse import dok_matrix
-from project import BoolDecomposedNFA, graph_to_nfa, RSM, ECFG, Variable
+from project import BoolDecomposedNFA, graph_to_nfa, cfg_to_wcnf, RSM, ECFG, Variable
 from typing import Set, Tuple, Union
 
 __all__ = ["cfpq_by_tensor", "tensor", "cfpq"]
+
+
+def hellings(graph: MultiDiGraph, cfg: CFG) -> Set[Tuple]:
+
+    wcnf = cfg_to_wcnf(cfg)
+    eps_prods = set()
+    term_prods = {}
+    var_prods = {}
+
+    for p in wcnf.productions:
+        ph = p.head
+        pb = p.body
+        if len(pb) == 0:
+            eps_prods.add(ph)
+        elif len(pb) == 1:
+            if ph not in term_prods:
+                term_prods[ph] = set()
+            term_prods[ph].add(pb[0])
+        elif len(pb) == 2:
+            if ph not in var_prods:
+                var_prods[ph] = set()
+            var_prods[ph].add((pb[0], pb[1]))
+
+    result = set()
+
+    for u, v, data in graph.edges(data=True):
+        label = data["label"]
+        for p in term_prods:
+            if Terminal(label) in term_prods[p]:
+                result.add((u, p, v))
+
+    for v in graph.nodes:
+        for p in eps_prods:
+            result.add((v, p, v))
+
+    queue = result.copy()
+    while len(queue) > 0:
+        s, label, f = queue.pop()
+
+        temp = set()
+
+        for u, p, v in result:
+            if v == s:
+                for curr in var_prods:
+                    if (p, label) in var_prods[curr] and (u, curr, f) not in result:
+                        queue.add((u, curr, f))
+                        temp.add((u, curr, f))
+            if u == f:
+                for curr in var_prods:
+                    if (label, p) in var_prods[curr] and (s, curr, v) not in result:
+                        queue.add((s, curr, v))
+                        temp.add((s, curr, v))
+
+        result = result.union(temp)
+
+    return result
+
+
+def cfpq_by_hellings(
+    cfg: CFG,
+    graph: MultiDiGraph,
+    start_symbol: Variable = Variable("S"),
+    start_nodes: Set = None,
+    final_nodes: Set = None,
+) -> Set[Tuple[any, any]]:
+    return cfpq(cfg, graph, start_symbol, start_nodes, final_nodes, hellings)
 
 
 def tensor(graph: MultiDiGraph, cfg: CFG) -> Set[Union[int, str, int]]:
